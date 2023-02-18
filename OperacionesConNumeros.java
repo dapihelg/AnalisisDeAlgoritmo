21b48f6013866ea5732a1d525dff9134e163fa5e
Salvadorense <125716132+Salvadorense@users.noreply.github.com>
2023-02-17 22:58:10 -0600

    Add files via upload

---
+++ OperacionesConNumeros.java
@@ -0,0 +1,17 @@
+package OperacionesConNumeros;
+
+public class OperacionesConNumeros {
+
+    public OperacionesConNumeros(int suma, int suma2, int nums, int nums2) {
+        this.suma = suma;
+        this.suma2 = suma2;
+        this.nums = nums;
+        this.nums2 = nums2;
+    }
+public int sumandonums(){
+    int[]nums = new int[10];
+    
+}
+    
+    
+}
