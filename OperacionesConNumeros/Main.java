8f9114e17e05f4ab27a4db08d289c213371f3e96
dapihelg <xlx-1@hotmail.com>
2023-02-18 00:22:03 -0600

    desarrollo del programa en main

--- OperacionesConNumeros/Main.java
+++ OperacionesConNumeros/Main.java
@@ -13,27 +13,46 @@ public class Main {
 
         int numero = 0;
 
-        int resp = 0, resp2= 0, resp3 = 0, resp4 = 0;
-        String resp5="";
+        int resp = 0, resp2= 0, resp3 = 0;
+        String resp5="", resp4="";
 
 
+        System.out.println("elige una opción"+" 1.- suma con for o 2.- multiplicación ");
+        resp3 = in.nextInt();
+        if(resp3 == 1) {
+            do {
 
-        do {
+                System.out.println("Número que desea sumar ");
+                nums = in.nextInt();
 
-            System.out.println("Número que desea sumar ");
-            nums = in.nextInt();
+                System.out.println("Número de veces que desea sumar el número ");
+                resp = in.nextInt();
+                OperacionesConNumeros opn = new OperacionesConNumeros(suma, suma2, nums, nums2, numero, resp, resp2);
 
-            System.out.println("Número de veces que desea sumar el número ");
-            resp = in.nextInt();
-            OperacionesConNumeros opn = new OperacionesConNumeros( suma, suma2, nums, nums2, numero,resp, resp2);
-
-                for(int i=0; i<resp; i++) {
+                for (int i = 0; i < resp; i++) {
                     suma += nums;
                 }
 
-            System.out.println("el resultado es: "+suma);
+                System.out.println("el resultado es: " + suma);
 
-        } while(resp5.equalsIgnoreCase("si"));
+            } while (resp5.equalsIgnoreCase("si"));
+        }else{
+
+            do {
+
+                System.out.println("Número que desea multiplicar ");
+                nums2 = in.nextInt();
+
+                System.out.println("Número de veces que desea multiplicar el número ");
+                resp2 = in.nextInt();
+                OperacionesConNumeros opn = new OperacionesConNumeros(suma, suma2, nums, nums2, numero, resp, resp2);
+
+                suma2 = nums2*resp2;
+
+                System.out.println("el resultado es: " + suma2);
+
+            } while (resp4.equalsIgnoreCase("si"));
+        }
 
     }
 
